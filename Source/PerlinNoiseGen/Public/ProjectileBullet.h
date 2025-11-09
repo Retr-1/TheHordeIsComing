8c5fe3e525b36021baa5da751f2ebb58a9d908cd
Martin Bakus <136487973+Retr-1@users.noreply.github.com>
2025-11-09 18:02:19 +0100

    removed on overlap declaration

--- Source/PerlinNoiseGen/Public/ProjectileBullet.h
+++ Source/PerlinNoiseGen/Public/ProjectileBullet.h
@@ -38,11 +38,6 @@ protected:
         FVector NormalImpulse, const FHitResult& Hit);
 
     UFUNCTION()
-    void OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-        bool bFromSweep, const FHitResult& SweepResult);
-
-    UFUNCTION()
     void OnProjectileStop(const FHitResult& ImpactResult);
 
 private:
